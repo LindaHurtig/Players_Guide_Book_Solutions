cbdf790f6c3796a2a59d9eedc662a90a129f0d7d
LindaHurtig <92912834+LindaHurtig@users.noreply.github.com>
2024-10-01 16:47:58 +0200

    Add files via upload

---
+++ ReparingTheClocktower/ReparingTheClocktower/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int input;
+
+Console.WriteLine("Write a number: ");
+input = Convert.ToInt16(Console.ReadLine());
+
+if(input % 2 == 0)
+{
+    Console.WriteLine("Tick");
+}
+else
+{
+    Console.WriteLine("Tock");
+}
