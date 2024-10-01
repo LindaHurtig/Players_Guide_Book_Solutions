2a1a72fc0cfd07edf7f9a5fdc3ddbc57d7210f84
LindaHurtig <92912834+LindaHurtig@users.noreply.github.com>
2024-10-01 16:44:58 +0200

    Add files via upload

---
+++ TheDiffensOfConsolas/TheDiffensOfConsolas/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Title = "The Diffens of Consolas";
+
+int row;
+int column;
+
+Console.WriteLine("Target Row?");
+row = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Target Column?");
+column = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Target Row " + row + " Column " + column);
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine("Deploy to:");
+Console.WriteLine($"({ row }, { column - 1 })"); //Left Coord
+Console.WriteLine($"({row - 1}, {column})");  // Buttom Coord
+Console.WriteLine($"({row}, {column + 1})"); // Right Coord
+Console.WriteLine($"({row + 1}, {column})"); // Top Coord
+
+Console.Beep();
+
+Console.ForegroundColor = ConsoleColor.White;
+
+
+
