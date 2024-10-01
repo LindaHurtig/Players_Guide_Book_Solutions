c6fa83a509def6b87122d91687cb8687cf7e257f
LindaHurtig <92912834+LindaHurtig@users.noreply.github.com>
2024-10-01 16:53:37 +0200

    Add files via upload

---
+++ The Point/The Point/Program.cs
@@ -0,0 +1,24 @@
+﻿Point a = new Point(2, 3);
+Point b = new Point(-4, 0);
+
+Console.WriteLine($"{a._x}, {a._y}");
+Console.WriteLine($"{b._x}, {b._y}");
+
+public class Point
+{
+    public float _x { get; }
+    public float _y {get;}
+
+    public Point(float x, float y)
+    {
+        _x = x;
+        _y = y;
+    }
+
+    public Point() 
+    {
+        _x = 0;
+        _y = 0;
+    }
+
+}
