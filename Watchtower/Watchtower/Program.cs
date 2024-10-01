07f78b822ebab6148f3d0017e169ab5e555b17fb
LindaHurtig <92912834+LindaHurtig@users.noreply.github.com>
2024-10-01 16:48:47 +0200

    Add files via upload

---
+++ Watchtower/Watchtower/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string directionX = "";
+string directionY = "";
+
+Console.WriteLine("Write an X value:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Write an Y value:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x == 0 && y == 0){
+    Console.WriteLine("The enemy is here!");
+}
+else 
+{ 
+    if (x != 0)
+    {
+       if (x < 0)
+        {
+            directionX = "W";
+        }
+        else if (x > 0)
+        {
+            directionX = "E";
+        }
+    }
+
+    if (y != 0)
+    {    
+        if (y < 0)
+        {
+            directionY = "S";
+        }
+        else if ( y > 0)
+        {
+            directionY = "N";
+        }
+
+    }
+
+    Console.WriteLine("Enemy is coming from " + directionY + directionX);
+
+}
+
+
+
+
+
+
+
